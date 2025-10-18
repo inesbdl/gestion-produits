dfdab0f9ce8a557c7bf2622ff6a360a09e627081
inesbdl <bouadilin@gmail.com>
2025-10-18 20:25:48 +0200

    Initial commit - projet cloné et adapté pour mon repo perso

--- php/www/connect.php
+++ php/www/connect.php
@@ -1,12 +1,20 @@
 <?php
-    $host = "db";
-    $username = "root";
-    $password = "root";
-    $db = "gestion_produits";
+$host = "db";
+$username = "root";
+$password = "root";
+$dbname = "gestion_produits";
 
-    // Connexion avec pdo mysql
-    $db = new PDO("mysql:host=$host;dbname=$db", $username, $password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    
+$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::MYSQL_ATTR_SSL_CA => false
+];
+
+try {
+    $db = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
 ?>
